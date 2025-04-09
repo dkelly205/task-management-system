f43d9190585793c284e8fcca75d1b66efd5997ba
dkelly205 <danieljkelly91@gmail.com>
2025-04-10 00:09:44 +0100

    adds valid annotation to register endpoint

--- src/main/java/com/dkelly205/task_management_system/controller/AuthController.java
+++ src/main/java/com/dkelly205/task_management_system/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.dkelly205.task_management_system.dto.JwtAuthResponse;
 import com.dkelly205.task_management_system.dto.LoginDto;
 import com.dkelly205.task_management_system.dto.RegisterDto;
 import com.dkelly205.task_management_system.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterDto registerDto){
         String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
